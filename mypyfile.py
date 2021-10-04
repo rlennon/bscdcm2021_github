fa6a1b3f59371941e62fe91e8da487c6f56dcf05
R Lennon <ruth.lennon@lyit.ie>
2021-10-04 20:22:15 +0100

    Files updated. Error removed

--- mypyfile.py
+++ mypyfile.py
@@ -1,11 +1,5 @@
 
 print("Hello lyit")
 print("Hello again...")
-<<<<<<< HEAD
 print("The time is 20:01 GMT DST")
 print("Hello from github to your local repo. Don't forget to pull from the repo")
-
-Oh dear this is not a line I want to keep!
-=======
-print("The time is 20:01 GMT DST")
->>>>>>> parent of 5d952a1 (Updated mypyfile.py with hello from github)
